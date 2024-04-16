04e93ddd8a26512c4e60152b7dd329c58a3e1435
s31606 <162446202+s31606@users.noreply.github.com>
2024-04-16 12:16:34 +0200

    PGO5

--- PGO5/src/Employee.java
+++ PGO5/src/Employee.java
@@ -1,5 +1,26 @@
+import java.time.Year;
+
 public class Employee {
+    protected String name;
+    protected String lastName;
+    protected String address;
+    protected String email;
+    protected String pesel;
+    protected int hireYear;
+
+    public Employee(String name, String lastName, String address, String email, String pesel, int hireYear) {
+        this.name = name;
+        this.lastName = lastName;
+        this.address = address;
+        this.email = email;
+        this.pesel = pesel;
+        this.hireYear = hireYear;
+    }
+
     public int calculateSalary() {
-        return 0;
+        int baseSalary = 3000;
+        int yearsWorked = Year.now().getValue() - hireYear;
+        int salary = baseSalary + (yearsWorked * 1000);
+        return salary;
     }
 }
