5f4d00f0a087c2f9066a8d11d72b47b4ae56e309
s31606 <162446202+s31606@users.noreply.github.com>
2024-04-16 12:14:38 +0200

    Update Developer.java

--- PGO5/src/Developer.java
+++ PGO5/src/Developer.java
@@ -1,8 +1,23 @@
+import java.util.ArrayList;
+
 public class Developer extends Employee {
-    public Developer(String name, String lastName, String address, String mail, String pesel, int yearOfEmployment) {
+    private ArrayList<Technology> technologies = new ArrayList<>();
+
+    public Developer(String name, String lastName, String address, String email, String pesel, int hireYear) {
+        super(name, lastName, address, email, pesel, hireYear);
     }
 
     public void addTechnology(Technology t) {
+        technologies.add(t);
+    }
 
+    @Override
+    public int calculateSalary() {
+        int baseSalary = super.calculateSalary();
+        int bonus = 0;
+        for (Technology t : technologies) {
+            bonus += t.getBonus();
+        }
+        return baseSalary + bonus;
     }
 }
