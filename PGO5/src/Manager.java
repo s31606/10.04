62daa0d3c038a871334f8fcce0d83c8c2febd1d7
s31606 <162446202+s31606@users.noreply.github.com>
2024-04-16 12:18:25 +0200

    PGO5

--- PGO5/src/Manager.java
+++ PGO5/src/Manager.java
@@ -1,8 +1,29 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class Manager extends Employee{
-    public Manager(String name, String lastName, String address, String mail, String pesel, int yearOfEmployment) {
+    private ArrayList<Goal> goals = new ArrayList<>();
+
+    public Manager(String name, String lastName, String address, String email, String pesel, int hireYear) {
+        super(name, lastName, address, email, pesel, hireYear);
     }
 
     public void addGoals(Goal g) {
+        goals.add(g);
+    }
 
+    @Override
+    public int calculateSalary() {
+        int baseSalary = super.calculateSalary();
+        int bonus = 0;
+        LocalDate currentDate = LocalDate.now();
+
+        for (Goal goal : goals) {
+            if (goal.getYear() == currentDate.getYear() && goal.getMonth () == currentDate.getMonthValue()) {
+                bonus += goal.getBonus();
+            }
+        }
+
+        return baseSalary + bonus;
     }
 }
