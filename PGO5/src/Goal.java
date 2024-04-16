7243c51583970e521207427bfa3655cdc617548d
s31606 <162446202+s31606@users.noreply.github.com>
2024-04-16 12:17:06 +0200

    PGO5

--- PGO5/src/Goal.java
+++ PGO5/src/Goal.java
@@ -1,4 +1,27 @@
 public class Goal {
+    private int year;
+    private int month;
+    private int day;
+    private String name;
+    private int bonus;
+
     public Goal(int year, int month, int day, String name, int bonus) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+        this.name = name;
+        this.bonus = bonus;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getBonus() {
+        return bonus;
     }
 }
